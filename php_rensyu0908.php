9d8a400648093f81809a4bf09080678cd1918eb7
DESKTOP-K9354FR\root <kimura19870828@gmaill.com>
2016-09-09 13:58:32 +0900

    初回

---
+++ php_rensyu0908.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div>
+<?php
+
+$user='testuser';
+$password = 'testuser';
+$dbName = 'reservation';
+$host = 'localhost:3306';
+$dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+try{
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "データベース{$dbName}に接続しました";
+    echo "予約状況";
+
+    $today = date("Y/m/d");
+
+    //$sql1 = "SELECT id, name FROM room";
+    //$sql = "SELECT *  FROM reservation";
+    $sql = "SELECT reservation.date, 
+                reservation.room as rroom,
+                 reservation.room as rtimezone,
+                 reservation.check,
+                reservation.user,
+                reservation.branch,
+                reservation.text,
+                room.name,
+                timezone.hour
+            FROM reservation, room, timezone
+            WHERE reservation.room = room.id 
+            AND reservation.timezone=timezone.timezone
+       
+            ORDER BY reservation.date, reservation.room, 
+            reservation.timezone";
+
+     $stm = $pdo->prepare($sql);
+    $stm->execute();
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table border='1'>";
+    echo "<thead><tr bgcolor='yellow'>";
+    echo "<th>日付</th>";
+    echo "<th>時間</th>";
+    echo "<th>会議室</th>";
+    echo "<th>予約者</th>";
+    echo "<th>部署</th>";
+    echo "<th>目的</th>";
+    echo "</tr></thead><tbody>";
+
+    echo "<tbody>";
+
+    #$date = array_shift($result);
+    #var_dump($date);
+    foreach ($result as $row) {
+        # if($date == ($row['date'])) {
+        #    echo "<tr bgcolor='white'>";
+        #}else{
+        #    echo "<tr bgcolor='grey'>";
+        #}
+            echo "<tr>";
+            echo "<td>" . ($row['date']) . "</td>";
+            echo "<td>" . ($row['hour']) . "</td>";
+            echo "<td>" . ($row['name']) . "</td>";
+            echo "<td>" . ($row['user']) . "</td>";
+            echo "<td>" . ($row['branch']) . "</td>";
+            echo "<td>" . ($row['text']) . "</td>";
+            echo "</tr>";
+            $date = $row['date'];
+        }
+    echo "</tbody>";
+    echo "</table>";
+
+}catch(Exception $e){
+    echo '<span class="error">エラーがありました</span>';
+    echo $e->getMessage();
+    exit();
+}
+?>
+</div>
+</body>
+</html>
